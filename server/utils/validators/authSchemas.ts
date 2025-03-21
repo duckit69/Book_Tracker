48e4bc685beebb71f391a414cf11c268d6e7bdbc
duckit 69 <gherab.radouane@gmail.com>
2025-03-22 00:35:21 +0100

    feat: Added userSignupSchemaValidator

--- server/utils/validators/authSchemas.ts
+++ server/utils/validators/authSchemas.ts
@@ -4,4 +4,5 @@ const userSignupSchema = Joi.object({
   username: Joi.string().alphanum().required(),
   password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
   repeat_password: Joi.ref("password"),
+  birthDate: Joi.date().max("now").greater("1950-01-01").required(),
 });
